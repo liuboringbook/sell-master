2b24795d88f15e6d3b7b230207128f8b1f9e3aa6
read a book <1287128847@qq.com>
2018-03-26 20:17:19 +0800

    sell-master

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+
+  <div id="app">
+   <v-header id=""></v-header>
+    <div class="tab">
+      <div class="tab-item"><a v-link="{path: '/goods'}">商品</a></div>
+      <div class="tab-item"><a v-link="{path: '/ratings'}">评价</a></div>
+      <div class="tab-item"><a v-link="{path: '/seller'}">商家</a></div>
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script>
+import vHeader from './components/header.vue'
+export default {
+  name: 'App',
+  components: {
+    vHeader
+  }
+}
+</script>
+
+<style>
+.tab{
+  width:100%;
+  display: flex;
+  flex-direction: row;
+  height:40px;
+  line-height: 40px;
+}
+.tab-item{
+  flex: 1;
+  text-align: center;
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+    <div class="header">
+      我是Header
+    </div>
+</template>
+<script>
+export default {}
+</script>
+<style>
+
+</style>
